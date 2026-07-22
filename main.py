56bf2a9350c189ecd2469a1716c9d83ed82d71bc
Rajasekaran Paramasivam <rajasekaran@rparama.com>
2026-07-22 08:38:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import requests
 from twilio.rest import Client
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-AUTH_TOKEN = os.environ.get("OWM_API_KEY")
+OWM_API_KEY = os.environ.get("OWM_API_KEY")
 ACCOUNT_SID = os.environ.get("ACCOUNT_SID")
 AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
 weather_params = {
     "lat"   :   19.075983,
     "lon"   :   72.877655,
-    "appid" :   api_key,
+    "appid" :   OWM_API_KEY,
     "cnt"   :   4
 }
-print(api_key, account_sid, auth_token)
+print(OWM_API_KEY, ACCOUNT_SID, AUTH_TOKEN,weather_params)
 response = requests.get(url=OWM_Endpoint, params=weather_params)
 print(response.status_code)
 response.raise_for_status()
@@ -25,7 +25,7 @@ for hour_data in weather_data["list"]:
         will_rain = True
 
 if will_rain:
-    client = Client(account_sid, auth_token)
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
     message = client.messages.create(
         from_='+12603006548',
         body="It's going to rain 🌧️ today. Remember to bring an ☔️",
